d63cc4a56a3543d8167a2416fd0bb879d88f2e98
xujiping <xjpdyx@hotmail.com>
2017-11-21 16:39:16 +0800

    1、增加元素定制页面

--- cms-admin/src/main/java/com/xjp/config/ShiroConfiguration.java
+++ cms-admin/src/main/java/com/xjp/config/ShiroConfiguration.java
@@ -28,7 +28,7 @@ public class ShiroConfiguration {
         //必须设置SecurityManager
         shiroFilterFactoryBean.setSecurityManager(securityManager);
         //如果不设置默认会自动寻找根目录/login.jsp页面
-        shiroFilterFactoryBean.setLoginUrl("/login");
+        shiroFilterFactoryBean.setLoginUrl("/manage/login");
         //登录成功后跳转
         shiroFilterFactoryBean.setSuccessUrl("/manage/index");
         //未授权界面

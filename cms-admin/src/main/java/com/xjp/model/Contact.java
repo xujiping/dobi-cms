310cad9e4c34fd2f9cafed342924bc470b13759a
daite <851980336@qq.com>
2017-11-21 23:55:53 +0800

    *完成联系我们前端

---
+++ cms-admin/src/main/java/com/xjp/model/Contact.java
@@ -0,0 +1,120 @@
+package com.xjp.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Table(name = "article")
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 日期
+     */
+    private Date date;
+
+    /**
+     * 状态：1可用 0不可用
+     */
+    private Integer status;
+
+    /**
+     * 内容
+     */
+    private String content;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取标题
+     *
+     * @return title - 标题
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * 设置标题
+     *
+     * @param title 标题
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * 获取日期
+     *
+     * @return date - 日期
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * 设置日期
+     *
+     * @param date 日期
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    /**
+     * 获取状态：1可用 0不可用
+     *
+     * @return status - 状态：1可用 0不可用
+     */
+    public Integer getStatus() {
+        return status;
+    }
+
+    /**
+     * 设置状态：1可用 0不可用
+     *
+     * @param status 状态：1可用 0不可用
+     */
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    /**
+     * 获取内容
+     *
+     * @return content - 内容
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * 设置内容
+     *
+     * @param content 内容
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
--- cms-admin/src/main/java/com/xjp/web/ContactController.java
+++ cms-admin/src/main/java/com/xjp/web/ContactController.java
@@ -1,9 +1,15 @@
 package com.xjp.web;
 
+import com.xjp.dao.MenuMapper;
+import com.xjp.model.Menu;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "contact")
@@ -11,13 +17,18 @@ public class ContactController {
 
     private Logger _LOGGER = LoggerFactory.getLogger(IndexController.class);
 
+    @Autowired
+    private MenuMapper menuMapper;
+
     /**
      * contactMe.
      *
      * @return index.html
      */
     @RequestMapping(value = {"", "/index"})
-    public String contact() {
+    public String contact(Model model) {
+        List<Menu> menus = menuMapper.selectAll();
+        model.addAttribute("menus", menus);
         return "front/contact";
     }
 }

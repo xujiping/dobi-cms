2e1d8944046cc9be48e160f02303dfb66fc1d4b1
Jerry <173773787@qq.com>
2017-11-22 21:59:07 +0800

    EE

--- cms-admin/src/main/java/com/xjp/web/AboutUsController.java
+++ cms-admin/src/main/java/com/xjp/web/AboutUsController.java
@@ -1,14 +1,25 @@
 package com.xjp.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.xjp.dao.MenuMapper;
+import com.xjp.model.Menu;
 
 @Controller
 public class AboutUsController {
-	
+
+	@Autowired
+	private MenuMapper menuMapper;
+
 	@RequestMapping("/aboutUs")
-	public String aboutUs(){
-		
+	public String aboutUs(Model model) {
+		List<Menu> menus = menuMapper.selectAll();
+		model.addAttribute("menus", menus);
 		return "front/about-us";
 	}
 }

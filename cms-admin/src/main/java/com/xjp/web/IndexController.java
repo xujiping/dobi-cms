f56c56bcdc3003c8db0e4c4053a304dbef422afd
dthat <851980336@qq.com>
2017-11-24 16:43:43 +0800

    文章管理添加封面路径配置，整入试管资讯模块

--- cms-admin/src/main/java/com/xjp/web/IndexController.java
+++ cms-admin/src/main/java/com/xjp/web/IndexController.java
@@ -1,17 +1,13 @@
 package com.xjp.web;
 
-import com.xjp.dao.ContactMapper;
-import com.xjp.common.constants.ResultConstants;
-import com.xjp.common.result.Result;
 import com.xjp.dao.ArticleMapper;
+import com.xjp.dao.ContactMapper;
 import com.xjp.dao.MenuMapper;
 import com.xjp.dao.UploadMapper;
-import com.xjp.model.Contact;
 import com.xjp.model.Article;
 import com.xjp.model.Menu;
 import com.xjp.model.Upload;
 import com.xjp.service.MenuService;
-
 import org.apache.ibatis.session.RowBounds;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,23 +15,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileInputStream;
 import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * index.
@@ -84,89 +71,6 @@ public class IndexController {
         return "front/index";
     }
 
-//    /**
-//     * about.
-//     *
-//     * @return about.html
-//     */
-//    @RequestMapping(value = "/about")
-//    public String about(Model model) {
-//        List<Menu> menus = menuMapper.selectAll();
-//        model.addAttribute("menus", menus);
-//        List<Upload> bigImages = uploadMapper.selectUploadByElementId(1);
-//        model.addAttribute("bigImages", bigImages);
-//        return "front/about";
-//    }
-
-    /**
-     * service.
-     *
-     * @return service.html
-     */
-    @RequestMapping(value = "/service")
-    public String service(Model model) {
-        List<Menu> menus = menuMapper.selectAll();
-        model.addAttribute("menus", menus);
-        List<Upload> bigImages = uploadMapper.selectUploadByElementId(1);
-        model.addAttribute("bigImages", bigImages);
-        return "front/service";
-    }
-//
-//    /**
-//     * success.
-//     *
-//     * @return success.html
-//     */
-//    @RequestMapping(value = "/success")
-//    public String success(Model model) {
-//        List<Menu> menus = menuMapper.selectAll();
-//        model.addAttribute("menus", menus);
-//        List<Upload> bigImages = uploadMapper.selectUploadByElementId(1);
-//        model.addAttribute("bigImages", bigImages);
-//        return "front/success";
-//    }
-
-    /**
-     * tube.
-     *
-     * @return tube.html
-     */
-    @RequestMapping(value = "/tube")
-    public String tube(Model model) {
-        List<Menu> menus = menuMapper.selectAll();
-        model.addAttribute("menus", menus);
-        List<Upload> bigImages = uploadMapper.selectUploadByElementId(1);
-        model.addAttribute("bigImages", bigImages);
-        return "front/tube";
-    }
-
-//    /**
-//     * message.
-//     *
-//     * @return message.html
-//     */
-//    @RequestMapping(value = "/message")
-//    public String message(Model model) {
-//        List<Menu> menus = menuMapper.selectAll();
-//        model.addAttribute("menus", menus);
-//        List<Upload> bigImages = uploadMapper.selectUploadByElementId(1);
-//        model.addAttribute("bigImages", bigImages);
-//        return "front/message";
-//    }
-
-//    /**
-//     * contact.
-//     *
-//     * @return contact.html
-//     */
-//    @RequestMapping(value = "/contact")
-//    public String contact(Model model) {
-//        List<Menu> menus = menuMapper.selectAll();
-//        model.addAttribute("menus", menus);
-//        Contact contact = contactMapper.selectByPrimaryKey(1);
-//        model.addAttribute("contact", contact);
-//        return "front/contact";
-//    }
 
     /**
      * 读取上传的文件
@@ -199,13 +103,13 @@ public class IndexController {
      * @param limit
      * @return
      */
-    @GetMapping("/articles/{type}")
+    @GetMapping("/article/{type}")
     @ResponseBody
-    public Object articles(@PathVariable(name = "type") Integer type,
+    public Object article(@PathVariable(name = "type") Integer type,
                           @RequestParam(required = false, defaultValue = "0", value = "offset")
-                              int offset,
+                                  int offset,
                           @RequestParam(required = false, defaultValue = "10", value = "limit")
-                              int limit) {
+                                  int limit) {
         Article article = new Article();
         article.setType(type);
         RowBounds rowBounds = new RowBounds(offset, limit);
---
+++ cms-admin/src/main/java/com/xjp/web/front/TubeResult.java
@@ -0,0 +1,55 @@
+package com.xjp.web.front;
+
+import com.xjp.dao.ArticleMapper;
+import com.xjp.dao.MenuMapper;
+import com.xjp.dao.UploadMapper;
+import com.xjp.model.Menu;
+import com.xjp.model.Upload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * 试管资讯
+ *
+ * @author xujiping 2017-11-23 10:20
+ */
+@Controller
+@RequestMapping("tube")
+public class TubeResult {
+
+    private Logger _LOGGER = LoggerFactory.getLogger(TubeResult.class);
+
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    private MenuMapper menuMapper;
+
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    private UploadMapper uploadMapper;
+
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    private ArticleMapper articleMapper;
+
+    /**
+     * 菜单页面
+     *
+     * @return front/tube.html
+     */
+    @GetMapping("")
+    public String index(Model model) {
+        List<Menu> menus = menuMapper.selectAll();
+        model.addAttribute("menus", menus);
+        List<Upload> bigImages = uploadMapper.selectUploadByElementId(1);
+        model.addAttribute("bigImages", bigImages);
+        return "front/tube";
+    }
+
+}
